6e5066a16a69abfe9b3afd3d89b4b4889aebf0b9
Sava-Sav <et.sava.m@eduiteh.eu>
2026-02-03 09:32:54 +0200

    SecondSemester

---
+++ game.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById("gameCanvas");
+
+const context = canvas.getContext("2d")
+
+const playerWidth = 100;
+const playerHeight = 100;
+const playerColor = "green"
+let x = 400;
+let y = 300;
+
+function update() {
+    x = x + 5;
+
+    context.clearRect(0, 0, 800, 600)
+    
+context.fillStyle = playerColor;
+context.fillRect(
+    x - (playerWidth / 2),
+    y - (playerHeight / 2),
+    playerWidth,
+    playerHeight);
+    
+requestAnimationFrame(update);
+
+}
+
+update();
